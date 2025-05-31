3f6c0ae1e07c86b74873d519b23b380989900e25
raws6633 <raw@seishun.work>
2025-05-31 18:58:40 +0900

    update

---
+++ src/components/ClientSessionProvider.tsx
@@ -0,0 +1,9 @@
+// components/ClientSessionProvider.tsx
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
+
+export default function ClientSessionProvider({ children }: { children: ReactNode }) {
+    return <SessionProvider>{children}</SessionProvider>;
+}
